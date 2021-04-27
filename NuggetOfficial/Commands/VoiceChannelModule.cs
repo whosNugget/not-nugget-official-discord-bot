9621b53337b417bd6e4c3b265f65d7b3d553568b
whosNugget <19211255+whosNugget@users.noreply.github.com>
2021-04-26 18:29:46 -0600

    Initial implementation of the permission system
    lots of work still needs to be done...starting to become more and more blocked with the data storage mechanism...in-memory data is just fine for single server applications, but this can very easily be sharded and expanded for the public to use

--- NuggetOfficial/Commands/VoiceChannelModule.cs
+++ NuggetOfficial/Commands/VoiceChannelModule.cs
@@ -3,6 +3,7 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using NuggetOfficial.Data;
+using NuggetOfficial.Authority;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -23,11 +24,14 @@ namespace NuggetOfficial.Commands
 		readonly DiscordGuild registeredGuild = null;
 		readonly DiscordRole memberRole = null;
 		readonly DiscordRole mutedRole = null;
+		readonly VoiceChannelPermissions everyonePermission;
+
+		readonly Dictionary<DiscordRole, VoiceChannelPermissions> rolewisePermissions = new Dictionary<DiscordRole, VoiceChannelPermissions>();
+		readonly Dictionary<DiscordMember, VoiceChannelPermissions> memberwisePermissions = new Dictionary<DiscordMember, VoiceChannelPermissions>();
+		readonly Dictionary<DiscordMember, List<DiscordChannel>> createdChannels = new Dictionary<DiscordMember, List<DiscordChannel>>();
 
 		DiscordChannel parentCategory = null;
 		DiscordChannel waitingRoomVC = null;
-
-		Dictionary<DiscordMember, DiscordChannel> createdChannels = new Dictionary<DiscordMember, DiscordChannel>();
 
 		public VoiceChannelModule(DiscordGuild registeredGuild)
 		{
@@ -41,6 +45,8 @@ namespace NuggetOfficial.Commands
 				if (ConfigurationManager.AppSettings.AllKeys.Contains("mutedRole")) mutedRole = this.registeredGuild.GetRole(ulong.Parse(ConfigurationManager.AppSettings.Get("mutedRole")));
 
 				registeredGuild.GetChannel(832686904066834443).SendMessageAsync($"Registered this server's parent category to {parentCategory.Name}, waiting room to {waitingRoomVC.Name}, member role to {memberRole.Mention}, and muted role to {mutedRole.Mention}");
+
+				everyonePermission = new VoiceChannelPermissions();
 			}
 		}
 
@@ -65,48 +71,114 @@ namespace NuggetOfficial.Commands
 			}
 		}
 
-		//TODO write every other method parameter combination. If not all of them, at least the all default one (probably wont do)
-		[Command("createvc")]
-		public async Task CreateVC(CommandContext ctx, ChannelPublicity publicity = ChannelPublicity.Public, int? maxUsers = 0, int? bitrate = 64000, VoiceRegion region = VoiceRegion.Automatic, params DiscordMember[] permittedMembers)
+		[Command("permit")]
+		public async Task Permit(CommandContext ctx, DiscordRole role)
 		{
-			if (!(ctx.Member.VoiceState is null) && !(ctx.Member.VoiceState.Channel is null) && ctx.Member.VoiceState.Channel.Equals(waitingRoomVC))
+			//TODO allow server owners to specify moderator roles/members that can utilize these commands
+			if (ctx.Member.PermissionsIn(ctx.Channel).HasFlag(Permissions.ManageChannels))
 			{
-				//in waiting room vc
-				if (ValidateVCParameterInput(out string error, publicity, maxUsers, bitrate, region, permittedMembers))
+				if (!(role is null) && !rolewisePermissions.ContainsKey(role))
 				{
-					//input valid
-					if (!createdChannels.ContainsKey(ctx.Member) || (createdChannels.ContainsKey(ctx.Member) && createdChannels[ctx.Member] is null))
-					{
-						//member does not own a VC
-						List<DiscordOverwriteBuilder> channelPermissionsBuilderList = GeneratePermissionOverwrites(ctx.Member, publicity, permittedMembers);
-						await CreateChannelAndMoveMemberAsync(ctx.Member, maxUsers, bitrate, region, channelPermissionsBuilderList);
-
-						if ((publicity == ChannelPublicity.Private || publicity == ChannelPublicity.Hidden) && permittedMembers?.Length == 0)
-						{
-							//member created a private VC with no allowed members
-							await RespondAsync(ctx.Message, "``You've created a private channel but you haven't allowed members to join. Be sure to update the vc with a list of allowed members, else only you will be allowed to join``");
-						}
-					}
-					else
-					{
-						//member already owns a VC
-						await RespondAsync(ctx.Message, "``You need to delete your previously created channel before creating a new one``");
-					}
-				}
-				else
-				{
-					//input invalid
-					await RespondAsync(ctx.Message, error);
+					rolewisePermissions.Add(role, new VoiceChannelPermissions(ChannelCreationAuthority.Authorized, ChannelRenameAuthority.Authorized, ChannelCreationQuantityAuthority.Unlimited, ChannelAccesibilityConfigurationAuthority.Authorized, ChannelRegionConfigurationAuthority.Authorized));
 				}
 			}
 			else
 			{
-				//creator not in the waiting room
-				await RespondAsync(ctx.Message, $"``You cannot create a voice channel if you arent in the waiting room: {waitingRoomVC.Name}``");
+				await RespondAsync(ctx.Message, "You do not have permission to manage the channel, thus you are not allowed to permit roles or users");
+			}
+		}
+		[Command("permit")]
+		public async Task Permit(CommandContext ctx, DiscordMember member)
+		{
+			//TODO allow server owners to specify moderator roles/members that can utilize these commands
+			if (ctx.Member.PermissionsIn(ctx.Channel).HasFlag(Permissions.ManageChannels))
+			{
+				if (!(member is null) && !memberwisePermissions.ContainsKey(member))
+				{
+					memberwisePermissions.Add(member, new VoiceChannelPermissions(ChannelCreationAuthority.Authorized, ChannelRenameAuthority.Authorized, ChannelCreationQuantityAuthority.Unlimited, ChannelAccesibilityConfigurationAuthority.Authorized, ChannelRegionConfigurationAuthority.Authorized));
+				}
+			}
+			else
+			{
+				await RespondAsync(ctx.Message, "You do not have permission to manage the channel, thus you are not allowed to permit roles or users");
+			}
+		}
+
+		[Command("createvc")]
+		public async Task CreateVC(CommandContext ctx, ChannelPublicity publicity = ChannelPublicity.Public, int? maxUsers = 0, int? bitrate = 64000, VoiceRegion region = VoiceRegion.Automatic, params DiscordMember[] permittedMembers)
+		{
+			//Validate the provided input
+			//error early if invalid
+
+			//Validate the member's permissions
+			//error early if invalid
+
+			//check if member is in a vc
+			//otherwise error early
+
+			//create the vc if this point was reached
+			string error = string.Empty;
+
+			if (!ValidateVCParameterInput(out error, publicity, maxUsers, bitrate, region, permittedMembers)) //ensure the provided parameters arent shite
+			{
+				await RespondAsync(ctx.Message, error);
+				return;
 			}
 
-			//dev message
-			await RespondAsync(ctx.Message, $"``publicity: {publicity}, maxUsers: {maxUsers}, bitrate: {bitrate}, region: {region}``");
+			VoiceChannelPermissions permissionValidator = GetPermissionValidationObjectAndExistingChannelsForMember(ctx.Member, out int existingChannels);
+			if (!permissionValidator.ValidateChannelCreationParameterAuthority(publicity, existingChannels, region, out error)) //ensure the member has the correct permissions to create the channel
+			{
+				await RespondAsync(ctx.Message, error);
+				return;
+			}
+
+			if (ctx.Member.VoiceState is null || ctx.Member.VoiceState.Channel is null || !ctx.Member.VoiceState.Channel.Equals(waitingRoomVC)) //ensure the member is in the waiting room
+			{
+				await RespondAsync(ctx.Message, $"``You cannot create a voice channel if you arent in the waiting room: {waitingRoomVC.Name}``");
+				return;
+			}
+
+			List<DiscordOverwriteBuilder> channelPermissionsBuilderList = GeneratePermissionOverwrites(ctx.Member, publicity, permittedMembers);
+			await CreateChannelAndMoveMemberAsync(ctx.Member, maxUsers, bitrate, region, channelPermissionsBuilderList);
+
+			//if (!(ctx.Member.VoiceState is null) && !(ctx.Member.VoiceState.Channel is null) && ctx.Member.VoiceState.Channel.Equals(waitingRoomVC))
+			//{
+			//	//in waiting room vc
+			//	if (ValidateVCParameterInput(out string error, publicity, maxUsers, bitrate, region, permittedMembers))
+			//	{
+			//		//input valid
+			//		if (!createdChannels.ContainsKey(ctx.Member) || (createdChannels.ContainsKey(ctx.Member) && createdChannels[ctx.Member] is null))
+			//		{
+			//			//member does not own a VC
+			//			List<DiscordOverwriteBuilder> channelPermissionsBuilderList = GeneratePermissionOverwrites(ctx.Member, publicity, permittedMembers);
+			//			await CreateChannelAndMoveMemberAsync(ctx.Member, maxUsers, bitrate, region, channelPermissionsBuilderList);
+
+			//			if ((publicity == ChannelPublicity.Private || publicity == ChannelPublicity.Hidden) && permittedMembers?.Length == 0)
+			//			{
+			//				//member created a private VC with no allowed members
+			//				await RespondAsync(ctx.Message, "``You've created a private channel but you haven't allowed members to join. Be sure to update the vc with a list of allowed members, else only you will be allowed to join``");
+			//			}
+			//		}
+			//		else
+			//		{
+			//			//member already owns a VC
+			//			await RespondAsync(ctx.Message, "``You need to delete your previously created channel before creating a new one``");
+			//		}
+			//	}
+			//	else
+			//	{
+			//		//input invalid
+			//		await RespondAsync(ctx.Message, error);
+			//	}
+			//}
+			//else
+			//{
+			//	//creator not in the waiting room
+			//	await RespondAsync(ctx.Message, $"``You cannot create a voice channel if you arent in the waiting room: {waitingRoomVC.Name}``");
+			//}
+
+			////dev message
+			//await RespondAsync(ctx.Message, $"``publicity: {publicity}, maxUsers: {maxUsers}, bitrate: {bitrate}, region: {region}``");
 		}
 
 		[Command("updatevc")]
@@ -116,32 +188,41 @@ namespace NuggetOfficial.Commands
 		}
 		[Command("updatevc")]
 		public async Task UpdateVC(CommandContext ctx, bool permit, params DiscordMember[] memberList)
-		{ 
+		{
 
 		}
 
 		[Command("deletevc")]
 		public async Task DeleveVC(CommandContext ctx)
 		{
-			if (createdChannels.ContainsKey(ctx.Member) && !(createdChannels[ctx.Member] is null))
+			if (!createdChannels.ContainsKey(ctx.Member) || (createdChannels.ContainsKey(ctx.Member) && (!(createdChannels[ctx.Member] is null) || createdChannels[ctx.Member].Count == 0)))
 			{
-				//member has a channel
-				try
-				{
-					DiscordChannel toDelete = createdChannels[ctx.Member];
-					await toDelete.DeleteAsync($"{ctx.Member.DisplayName}#{ctx.Member.Discriminator}:{ctx.Member.Id} requested to delete their created channel: {toDelete.Name}:{toDelete.Id}");
-					createdChannels[ctx.Member] = null;
-					await RespondAsync(ctx.Message, $"``Deleted channel {toDelete.Name}``");
-				}
-				catch(Exception e)
-				{
-					await RespondAsync(ctx.Message, $"``{e}``");
-				}
-			}
-			else
-			{
-				//member doesnt have a channel
 				await RespondAsync(ctx.Message, "``You need to create a VC to delete one``");
+				return;
+			}
+
+			try
+			{
+				DiscordChannel toDelete = createdChannels[ctx.Member].Count == 1 ? createdChannels[ctx.Member][0] : ctx.Member.VoiceState?.Channel;
+				if (toDelete is null)
+				{
+					await RespondAsync(ctx.Message, $"``Because you have multiple created channels, you must be in the VC you wish to delete``");
+					return;
+				}
+
+				if (!createdChannels[ctx.Member].Contains(toDelete))
+				{
+					await RespondAsync(ctx.Message, $"``You are in a VC you do not own and, therefore, cannot delete it``");
+					return;
+				}
+
+				await toDelete.DeleteAsync($"{ctx.Member.DisplayName}#{ctx.Member.Discriminator}:{ctx.Member.Id} requested to delete their created channel: {toDelete.Name}:{toDelete.Id}");
+				createdChannels[ctx.Member].Remove(toDelete);
+				await RespondAsync(ctx.Message, $"``Deleted channel {toDelete.Name}``");
+			}
+			catch (Exception e)
+			{
+				await RespondAsync(ctx.Message, $"``{e}``");
 			}
 		}
 
@@ -158,8 +239,8 @@ namespace NuggetOfficial.Commands
 			{
 				createdChannel = await registeredGuild.CreateVoiceChannelAsync(channelCreator.Nickname ?? $"{channelCreator.DisplayName}'s VC", parentCategory, bitrate, maxUsers, permissions, $"Channel created via command by member {channelCreator.DisplayName}#{channelCreator.Discriminator}:{channelCreator.Id}");
 
-				if (createdChannels.ContainsKey(channelCreator)) createdChannels[channelCreator] = createdChannel;
-				else createdChannels.Add(channelCreator, createdChannel);
+				if (createdChannels.ContainsKey(channelCreator)) (createdChannels[channelCreator] ?? new List<DiscordChannel>()).Add(createdChannel);
+				else createdChannels.Add(channelCreator, new List<DiscordChannel>(new []{ createdChannel }));
 			}
 			catch (Exception)
 			{
@@ -221,6 +302,35 @@ namespace NuggetOfficial.Commands
 			}
 
 			return channelPermissionsBuilderList;
+		}
+
+		VoiceChannelPermissions GetPermissionValidationObjectAndExistingChannelsForMember(DiscordMember member, out int existingChannels)
+		{
+			VoiceChannelPermissions highestPrecedencePermissions = null;
+			existingChannels = (createdChannels.ContainsKey(member) ? createdChannels[member]?.Count : 0) ?? 0;
+
+			int highestRolePosition = -1;
+			DiscordRole highestRole = null;
+			foreach (DiscordRole role in member.Roles)
+			{
+				if (highestRolePosition < role.Position)
+				{
+					highestRole = role;
+					highestRolePosition = role.Position;
+				}
+			}
+			
+			if (rolewisePermissions.ContainsKey(highestRole))
+			{
+				highestPrecedencePermissions = rolewisePermissions[highestRole];
+			}
+
+			if (memberwisePermissions.ContainsKey(member))
+			{
+				highestPrecedencePermissions = memberwisePermissions[member];
+			}
+
+			return highestPrecedencePermissions ?? everyonePermission;
 		}
 		#endregion
 	}
---
+++ NuggetOfficial/Permissions/VoiceChannelPermissions.cs
@@ -0,0 +1,236 @@
+﻿using NuggetOfficial.Data;
+
+namespace NuggetOfficial.Authority
+{
+	//TODO move these to their own files?
+	/// <summary>
+	/// The representation of a member or role's authority to create channels
+	/// </summary>
+	public enum ChannelCreationAuthority
+	{
+		/// <summary>
+		/// The represented member or role is not authorized to create channels
+		/// </summary>
+		Unauthorized,
+
+		/// <summary>
+		/// The represented member or role is authorized to create channels
+		/// </summary>
+		Authorized
+	}
+
+	/// <summary>
+	/// The representation of a member or role's authority to reaname created channels
+	/// </summary>
+	public enum ChannelRenameAuthority
+	{
+		/// <summary>
+		/// The represented member or role is not authorized to rename created channels
+		/// </summary>
+		Unauthorized,
+
+		/// <summary>
+		/// The represented member or role is not authorized to rename created channels
+		/// </summary>
+		Authorized
+	}
+
+	/// <summary>
+	/// The representation of a member or role's authority to create multiple channels
+	/// </summary>
+	public enum ChannelCreationQuantityAuthority
+	{
+		/// <summary>
+		/// The represented member or role is not authorized to create any voice channels
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// The represented member or role is authorized to create a single voice channel
+		/// </summary>
+		Single,
+
+		/// <summary>
+		/// The represented member or role is authorized to create a specific number of voice channels
+		/// </summary>
+		Specified,
+
+		/// <summary>
+		/// The represented member or role is authorized to create as many voice channels as they like
+		/// </summary>
+		Unlimited
+	}
+
+	/// <summary>
+	/// The representation of a member or role's authority to specify a channel's accesibility
+	/// </summary>
+	public enum ChannelAccesibilityConfigurationAuthority
+	{
+		/// <summary>
+		/// The represented member or role is not authorized to create a channel
+		/// </summary>
+		Unauthorized = 0,
+
+		/// <summary>
+		/// The represented member or role is authorized to create Public channels
+		/// </summary>
+		Public = 1,
+
+		/// <summary>
+		/// The represented member or role is authorized to create Private channels
+		/// </summary>
+		Private = 2,
+
+		/// <summary>
+		/// The represented member or role is authorized to create Supporter-Only channels
+		/// </summary>
+		Supporter = 4,
+
+		/// <summary>
+		/// The represented member or role is authorized to create Hidden channels
+		/// </summary>
+		Hidden = 8,
+
+		/// <summary>
+		/// The represented member or role is authorized to create Limited channels
+		/// </summary>
+		Limited = 16,
+
+		/// <summary>
+		/// The represented member or role is authorized to create any channel
+		/// </summary>
+		Authorized = Public | Private | Supporter | Hidden | Limited
+	}
+
+	/// <summary>
+	/// The representation of a member or role's authority to modify a channel's RTC region
+	/// </summary>
+	public enum ChannelRegionConfigurationAuthority
+	{
+		/// <summary>
+		/// The represented member or role is not authorized to modify a channel's RTC region
+		/// </summary>
+		Unauthorized,
+
+		/// <summary>
+		/// The represented member or role is authorized to modify a channel's RTC region
+		/// </summary>
+		Authorized
+	}
+
+	public class VoiceChannelPermissions
+	{
+		readonly ChannelCreationAuthority channelCreationAuthority;
+		readonly ChannelRenameAuthority channelRenameAuthority;
+		readonly ChannelCreationQuantityAuthority channelCreationQuantityAuthority;
+		readonly ChannelAccesibilityConfigurationAuthority channelAccesibilityConfigurationAuthority;
+		readonly ChannelRegionConfigurationAuthority channelRegionConfigurationAuthority;
+		readonly int specificChannelCreationQuantity;
+
+		/// <summary>
+		/// Create a default authorization scheme where the represented member or role is not authorized to do anything
+		/// </summary>
+		public VoiceChannelPermissions()
+		{
+			channelCreationAuthority = ChannelCreationAuthority.Unauthorized;
+			channelRenameAuthority = ChannelRenameAuthority.Unauthorized;
+			channelCreationQuantityAuthority = ChannelCreationQuantityAuthority.None;
+			channelAccesibilityConfigurationAuthority = ChannelAccesibilityConfigurationAuthority.Unauthorized;
+			channelRegionConfigurationAuthority = ChannelRegionConfigurationAuthority.Unauthorized;
+		}
+		/// <summary>
+		/// Create a custom authorization scheme
+		/// </summary>
+		/// <param name="channelCreationAuthority">Represented member or role's authorization to create channels</param>
+		/// <param name="channelCreationQuantityAuthority">Represented member or role's authorization to create multiple channels</param>
+		/// <param name="channelAccesibilityConfigurationAuthority">Represented member or role's authorization to configure a channel's accesibility</param>
+		/// <param name="channelRegionConfigurationAuthority">Represented member or role's authorization to configure a channel's voice server location</param>
+		/// <param name="specificChannelCreationQuantity">Number of channels the represented member can create. Only applies to members or roles with the <c>ChannelCreationQuantityAuthority.Specified</c> authority</param>
+		public VoiceChannelPermissions(ChannelCreationAuthority channelCreationAuthority, ChannelRenameAuthority channelRenameAuthority, ChannelCreationQuantityAuthority channelCreationQuantityAuthority, ChannelAccesibilityConfigurationAuthority channelAccesibilityConfigurationAuthority, ChannelRegionConfigurationAuthority channelRegionConfigurationAuthority, int specificChannelCreationQuantity = 1)
+		{
+			this.channelCreationAuthority = channelCreationAuthority;
+			this.channelRenameAuthority = channelRenameAuthority;
+			this.channelCreationQuantityAuthority = channelCreationQuantityAuthority;
+			this.channelAccesibilityConfigurationAuthority = channelAccesibilityConfigurationAuthority;
+			this.channelRegionConfigurationAuthority = channelRegionConfigurationAuthority;
+			this.specificChannelCreationQuantity = specificChannelCreationQuantity;
+		}
+
+		/// <summary>
+		/// Validate that the provided parameters adhere to this permission scheme
+		/// </summary>
+		/// <param name="requestedPublicity">The requested publicity of the channel to be created</param>
+		/// M<param name="existingChannels">The number of existing channels the represented member has, if any</param>
+		/// <param name="requestedRegion">The requested voice region to use for the channel to be created</param>
+		/// <param name="error">If a parameter does not adhere to this scheme, an error message will be generated</param>
+		/// <returns><c>true</c> if the parameters adhere to this permission scheme, <c>false</c> if otherwise</returns>
+		public bool ValidateChannelCreationParameterAuthority(ChannelPublicity requestedPublicity, int existingChannels, VoiceRegion requestedRegion, out string error)
+		{
+			error = string.Empty;
+
+			if (channelCreationAuthority == ChannelCreationAuthority.Unauthorized)
+			{
+				error = $"Member does not have the authority to create voice channels";
+				goto RequestedParametersInvalid;
+			}
+
+			if (!(channelCreationQuantityAuthority == ChannelCreationQuantityAuthority.Unlimited) && existingChannels > 0)
+			{
+				if (channelCreationQuantityAuthority == ChannelCreationQuantityAuthority.Single)
+				{
+					error = "Member does not have the authority to create multiple voice channels";
+					goto RequestedParametersInvalid;
+				}
+
+				if (channelCreationQuantityAuthority == ChannelCreationQuantityAuthority.Specified && ++existingChannels > specificChannelCreationQuantity)
+				{
+					error = "Member has already created the maximum number of allowed specific channels";
+					goto RequestedParametersInvalid;
+				}
+			}
+
+			if (channelRegionConfigurationAuthority != ChannelRegionConfigurationAuthority.Authorized && requestedRegion != VoiceRegion.Automatic)
+			{
+				error = "Member does not have the authority to change the voice channel's region";
+			}
+
+			switch (requestedPublicity)
+			{
+				case ChannelPublicity.Public:
+					if (!channelAccesibilityConfigurationAuthority.HasFlag(ChannelAccesibilityConfigurationAuthority.Public))
+					{
+						error = "Member does not have the authority to create public voice channels";
+						goto RequestedParametersInvalid;
+					}
+					break;
+
+				case ChannelPublicity.Private:
+					if (!channelAccesibilityConfigurationAuthority.HasFlag(ChannelAccesibilityConfigurationAuthority.Private))
+					{
+						error = "Member does not have the authority to create private voice channels";
+						goto RequestedParametersInvalid;
+					}
+					break;
+
+				case ChannelPublicity.Supporter:
+					if (!channelAccesibilityConfigurationAuthority.HasFlag(ChannelAccesibilityConfigurationAuthority.Supporter))
+					{
+						error = "Member does not have the authority to create supporter-only voice channels";
+						goto RequestedParametersInvalid;
+					}
+					break;
+
+				case ChannelPublicity.Hidden:
+					if (!channelAccesibilityConfigurationAuthority.HasFlag(ChannelAccesibilityConfigurationAuthority.Hidden))
+					{
+						error = "Member does not have the authority to create hidden voice channels";
+						goto RequestedParametersInvalid;
+					}
+					break;
+			}
+
+		RequestedParametersInvalid:
+			return error != string.Empty;
+		}
+	}
+}
