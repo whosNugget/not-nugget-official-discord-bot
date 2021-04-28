b122b2f0ef05da6bf175ec51625e1cc88b5933f4
whosNugget <19211255+whosNugget@users.noreply.github.com>
2021-04-28 15:59:49 -0600

    rename action class methods for clarity

--- NuggetOfficial/Actions/MemberValidation.cs
+++ NuggetOfficial/Actions/MemberValidation.cs
@@ -11,13 +11,13 @@ namespace NuggetOfficial.Actions
 {
 	public static class MemberValidation
 	{
-		public static async Task TrackMemberJoinGuild(DiscordClient member, GuildMemberAddEventArgs args)
+		public static async Task OnMemberJoinGuild(DiscordClient member, GuildMemberAddEventArgs args)
 		{
 			/*See if this member exists in the tracked information and perform an action if they violated a rule*/
 			await Task.CompletedTask;
 		}
 
-		public static async Task TrackMemberLeaveGuild(DiscordClient member, GuildMemberRemoveEventArgs args)
+		public static async Task OnMemberLeaveGuild(DiscordClient member, GuildMemberRemoveEventArgs args)
 		{
 			/*Should track information about the member as they leave, primarily if they were currently muted or soft-banned, and save it somewhere so information can be retrieved on rejoin in case they attemted to evade a mute/sban*/
 			await Task.CompletedTask;
