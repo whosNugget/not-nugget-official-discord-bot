2573b5566b90ea7eea9158bd6d2ad12f98c09b7b
whosNugget <19211255+whosNugget@users.noreply.github.com>
2021-06-29 18:16:29 -0600

    began stubbing settings object for voice channel module and decommissioned code that needs to be rewritten

---
+++ NuggetOfficialDiscord/Data/VoiceModule/ChannelCreationParameters.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using NuggetOfficial.Data;
+using System.Collections.Generic;
+
+namespace NuggetOfficial.Data.VoiceModule
+{
+    /// <summary>
+    /// Structure containing all parameters used for creating or editing channels, as well as some commonly useful metadata. Create instances of this structure using the initialization syntax
+    /// </summary>
+    public struct ChannelCreationParameters
+    {
+        public DiscordUser CreatingUser { get; }
+        public DiscordChannel InvocationChannel { get; }
+        public int ExistingChannels { get; }
+        public int MaximumAllowedChannels { get; }
+
+        public string ChannelName { get; }
+        public DiscordChannel ParentChannel { get; }
+        public int? Bitrate { get; }
+        public int? UserLimit { get; }
+        public IEnumerable<DiscordOverwrite> PermissionOverwrites { get; }
+        public ChannelPublicity Publicity { get; }
+        public VoiceRegion? RequestedRegion { get; }
+        public VideoQualityMode? VideoQuality { get; }
+        public string AuditLogReason { get; }
+    }
+}
---
+++ NuggetOfficialDiscord/Data/VoiceModule/VoiceChannelModuleSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuggetOfficialDiscord.Data.VoiceModule
+{
+    public class VoiceChannelModuleSettings
+    {
+        /// <summary>
+        /// Should members who are attempting to create VCs be required to execute commands within a specified channel? All command invocations will be met with an error or
+        /// will be ignored (configurable)
+        /// </summary>
+        public bool RequireOccupanceOfWaitingRoomVCWhenMemberChannelCreationAttemptOccurs { get; set; }
+
+        /// <summary>
+        /// Should commands recieved by members who are not in a specified waiting room be ignored?
+        /// </summary>
+        public bool IgnoreCommandInvocationWhenVCOccupanceConditionNotMet { get; set; }
+
+        /// <summary>
+        /// When wizard commands are used, should the voice module system create a DM channel with the caller and perform all wizard steps there? This can help reduce clutter,
+        /// but also takes the focus away from the server
+        /// </summary>
+        public bool CreateAndUseDMChannelsForWizards { get; set; }
+    }
+}
