9d6551df6922a2990a5172d8d32c72ce51170b03
whosNugget <19211255+whosNugget@users.noreply.github.com>
2021-07-07 22:03:35 -0600

    wizard pool empty class stub

---
+++ NuggetOfficialDiscord/Commands/VoiceHubModule/Wizard/Pool/WizardPool.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuggetOfficialDiscord.Commands.VoiceHubModule.Wizard
+{
+    public class WizardPool<T, TResult> where T : EmbedInteractionWizard<TResult> where TResult : struct, IWizardResult
+    {
+
+    }
+}
