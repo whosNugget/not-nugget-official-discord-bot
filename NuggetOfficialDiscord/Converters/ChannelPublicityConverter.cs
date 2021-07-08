f606d5f2ee3a45ad59722edbc05cf48544773ca5
whosNugget <19211255+whosNugget@users.noreply.github.com>
2021-07-08 14:51:47 -0600

    fixed converter implementation issue

---
+++ NuggetOfficialDiscord/Converters/ChannelPublicityConverter.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.Entities;
+using NuggetOfficial.Discord.Commands.VoiceHubModule.Data;
+using System.Threading.Tasks;
+
+namespace NuggetOfficial.Discord.Data.Converters
+{
+	public class ChannelPublicityConverter : IArgumentConverter<ChannelAccessibility>
+	{
+		public async Task<Optional<ChannelAccessibility>> ConvertAsync(string value, CommandContext ctx)
+		{
+			ChannelAccessibility output;
+
+			switch (value.ToLowerInvariant())
+			{
+				case "":
+				case "public":
+					output = ChannelAccessibility.Public;
+					break;
+
+				case "private":
+					output = ChannelAccessibility.Private;
+					break;
+
+				case "supporter":
+					output = ChannelAccessibility.Supporter;
+					break;
+
+				case "hidden":
+					output = ChannelAccessibility.Hidden;
+					break;
+
+				default:
+					output = ChannelAccessibility.Unknown;
+					break;
+			}
+
+			return await Task.FromResult(output);
+		}
+	}
+}
